abd016d8a1b1503ed087375c4585700184c1121b
Chinmay Chaudhari <chinmay@Chinmays-MacBook-Air.local>
2024-02-20 12:01:42 +0530

    4th commit

--- clientt/src/pages/Create.jsx
+++ clientt/src/pages/Create.jsx
@@ -1,34 +1,33 @@
 import React from "react";
 import dogPng from "../assets/dog.png";
-import "../pages/create.css";
-
-
+import "./create.css";
 function Create() {
   return (
-    <div className='minting'>
-        <h1 id="nft-title">Minting NFT</h1>
-        <div className="content">
-        <div className="left">
-            <div className='input-value'>
-                <label htmlFor="name">Name*</label>
-                <input type="text" placeholder='Name your NFT' id='name' />
-            </div>
-            <div className='input-value'>
-                <label htmlFor="Description">Description*</label>
-                <textarea type="text" placeholder='Describe your NFT' id='name' />
+    <div className=" flex  justify-center  ">
+      <img src={dogPng} alt="" className=" h-96 mr-48 mt-40" />
+      <div className=" justify-center">
+        <div
+          className=" text-4xl font-bold text-white ml-32 mt-60"
+          id="nft-title"
+        >
+          Create your NFTs
+        </div>
+        <div className="mt-10">
+            <label for="first_name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Name</label>
+            <input type="text" id="first_name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="John" required />
+        </div>
+        <div className="mt-10">
+            <label for="first_name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
+            <input type="text" id="first_name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="John" required />
+        </div>
 
-            </div>
-            <div className='input-value'>
-                <label htmlFor="supply">Supply*</label>
-                <input type="number" placeholder='1' id='supply' />
-            </div>
-        </div>
-        <div id="nft-title">
-            <h2>Create your NFT</h2>
-            <p>Once your item is Minted no changes can bee made </p>
-
-        </div>
-        </div>
+        <button
+          class="h-10 px-6 font-semibold rounded-md border border-slate-200 text-white mt-10"
+          type="button"
+        >
+          Browse
+        </button>
+      </div>
     </div>
   );
 }
